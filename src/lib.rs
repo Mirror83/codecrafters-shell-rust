855f84c0dc3ffaccbdcba0ae82a12c41e32a01ab
Mirror83 <glenochieng045@gmail.com>
2026-02-07 08:39:44 +0300

    refactor: add `Command` struct for handling shell commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,51 @@
 use std::io::{self, Write};
 
-enum EvalResultValue {
+enum CommandResultValue {
     Exit,
 }
 
-struct EvalError {
+struct CommandError {
     pub reason: String,
 }
 
-type EvalResult = Result<Option<EvalResultValue>, EvalError>;
+type CommandResult = Result<Option<CommandResultValue>, CommandError>;
 
 pub struct Shell {
     prompt_sign: String,
+}
+
+pub struct Command {
+    name: String,
+    // args: Vec<String>,
+}
+
+impl Command {
+    fn from_string(input: &String) -> Option<Command> {
+        let input = input.trim();
+
+        let tokens: Vec<String> = input
+            .split(" ")
+            .map(|token| token.trim().to_string())
+            .collect();
+
+        if tokens.len() == 0 {
+            return None;
+        }
+
+        Some(Command {
+            name: tokens[0].to_string(),
+            // args: tokens[1..].to_vec(),
+        })
+    }
+
+    fn run(&self) -> CommandResult {
+        match self.name.as_str() {
+            "exit" => Ok(Some(CommandResultValue::Exit)),
+            other => Err(CommandError {
+                reason: format!("{}: command not found", other),
+            }),
+        }
+    }
 }
 
 impl Shell {
@@ -23,8 +57,10 @@ impl Shell {
 
     pub fn run(&self) {
         loop {
-            let input = self.read();
-            let result = self.eval(&input);
+            let result = match self.read() {
+                Some(command) => self.eval(&command),
+                None => continue,
+            };
             self.print(&result);
             if self.should_exit(&result) {
                 break;
@@ -32,7 +68,7 @@ impl Shell {
         }
     }
 
-    fn read(&self) -> String {
+    fn read(&self) -> Option<Command> {
         print!("{} ", self.prompt_sign);
         io::stdout().flush().unwrap();
 
@@ -41,31 +77,25 @@ impl Shell {
             .read_line(&mut input)
             .expect("Failed to read input.");
 
-        input.trim().to_string()
+        Command::from_string(&input)
     }
 
-    fn eval(&self, input: &String) -> EvalResult {
-        if input == "exit" {
-            Ok(Some(EvalResultValue::Exit))
-        } else {
-            Err(EvalError {
-                reason: format!("{}: command not found", input),
-            })
-        }
+    fn eval(&self, command: &Command) -> CommandResult {
+        command.run()
     }
 
-    fn print(&self, result: &EvalResult) {
+    fn print(&self, result: &CommandResult) {
         match result {
             Ok(_) => {}
             Err(err) => println!("{}", err.reason),
         }
     }
 
-    fn should_exit(&self, result: &EvalResult) -> bool {
+    fn should_exit(&self, result: &CommandResult) -> bool {
         if let Ok(output) = result {
             if let Some(value) = output {
                 match value {
-                    EvalResultValue::Exit => return true,
+                    CommandResultValue::Exit => return true,
                 }
             }
         }
