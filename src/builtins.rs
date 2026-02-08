b107609b8fedc275e4838166a5bc3c21d0065909
Mirror83 <glenochieng045@gmail.com>
2026-02-08 17:34:05 +0300

    feat: add search for unknown commands in `PATH` to `print_type`
    this includes adding a crate (is_executable) for a clean way to
    to check if files in the PATH directories are executable

--- src/builtins.rs
+++ src/builtins.rs
@@ -1,4 +1,7 @@
+use std::fs::DirEntry;
+
 use crate::command::{CommandError, CommandResult, CommandResultValue};
+use is_executable::is_executable;
 
 pub enum Builtin {
     Echo,
@@ -35,8 +38,57 @@ pub fn print_type(args: &Vec<String>) -> CommandResult {
             "{} is a shell builtin",
             command_name
         )))),
-        None => Err(CommandError {
-            reason: format!("{}: not found", command_name),
-        }),
+        None => match search_path(command_name) {
+            Some(path) => Ok(Some(CommandResultValue::Output(format!(
+                "{} is {}",
+                command_name,
+                path.to_str().unwrap()
+            )))),
+            None => Err(CommandError {
+                reason: format!("{}: not found", command_name),
+            }),
+        },
     }
+}
+
+fn command_is_in_directory(command_name: &str, entry: &DirEntry) -> bool {
+    let entry_path = entry.path();
+    let Ok(metadata) = entry.metadata() else {
+        return false;
+    };
+    if metadata.is_file()
+        && (entry_path.file_name().unwrap() == command_name
+            || entry_path.file_stem().unwrap() == command_name)
+        && is_executable(&entry_path)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+fn search_path(command_name: &str) -> Option<std::path::PathBuf> {
+    let Ok(path) = std::env::var("PATH") else {
+        panic!("Could not get PATH environment variable values.")
+    };
+    for directory in std::env::split_paths(&path) {
+        match std::fs::read_dir(&directory) {
+            Ok(mut read_dir) => {
+                let Some(Ok(command_dir_entry)) =
+                    read_dir.find(|entry_result| match entry_result {
+                        Ok(entry) => command_is_in_directory(command_name, entry),
+                        Err(_) => false,
+                    })
+                else {
+                    continue;
+                };
+                let command_path = command_dir_entry.path();
+                return Some(command_path);
+            }
+            Err(_) => {
+                continue;
+            }
+        }
+    }
+    return None;
 }
